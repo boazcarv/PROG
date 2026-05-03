0b043a3e2a7e7327d38228c4098f5e44637c8ade
Pablo Boaz Carvalho Gomes <boazcarv@gmail.com>
2026-05-02 22:20:41 -0300

    cod

---
+++ ex/tests/script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuBtn && navLinks) {
+  menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
+
+  navLinks.querySelectorAll('a').forEach((link) => {
+    link.addEventListener('click', () => {
+      navLinks.classList.remove('open');
+    });
+  });
+}
