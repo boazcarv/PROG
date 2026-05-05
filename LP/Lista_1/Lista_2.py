9817827605235e817b6116a72afdd4585d0b08a3
Pablo Boaz Carvalho Gomes <boazcarv@gmail.com>
2026-05-05 17:36:27 -0300

    Update Lista_2.py

--- LP/Lista_1/Lista_2.py
+++ LP/Lista_1/Lista_2.py
@@ -14,8 +14,11 @@ elif salario <= 4664.68:
 else:
     aliquota = 0.275
 
+
 imposto = salario * aliquota
 print(f"Imposto a pagar: R$ {imposto:.2f}")
+#TEMOS OUTRA OPÇÃO DE DISFECHAR O PROBLEMA, USANDO O VALOR DO IMPOSTO PARA CADA FAIXA, COMO MOSTRA O CÓDIGO ABAIXO:
+#salario = float(input("Salário: R$ "))
 
 
 # Questão 2 - Ano bissexto
@@ -41,19 +44,20 @@ v1 = float(input("\nQ4) Digite o primeiro valor: "))
 v2 = float(input("Digite o segundo valor: "))
 
 if v1 > v2:
-    print(f"O maior valor é {v1}.")
+    print(f"O maior valor é: {v1:.2f}")
 elif v2 > v1:
-    print(f"O maior valor é {v2}.")
+    print(f"O maior valor é: {v2:.2f}")
 else:
     print("Os dois valores são iguais.")
 
 
 # Questão 5 - Diferença entre maior e menor (inteiros)
 
-num1 = int(input("\nQ5) Digite o primeiro número inteiro:"))
-num2 = int(input("Digite o segundo número intereiro:"))
+num1 = int(input("\nQ5) Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
 if num1 > num2:
     difenca = num1 - num2
-else:    difenca = num2 - num1
-print(f"A diferença entre o maior e o menor número é: {difenca}")  
+else:
+    difenca = num2 - num1
+print(f"A diferença entre o maior e o menor número é: {difenca}")
 
